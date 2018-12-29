33a0d3197d46805ccedbc12432da35f1fe79c86e
MaksGostroushko <makshostroyshko@gmail.com>
2018-12-29 14:34:06 +0200

    fixed comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,5 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :product
 
   # validates :body, presence: true, length: { maximum: 300 }
-  # default_sope { order(created_at: asc)}
+  # default_scope { order(created_at: asc)}
 end
