78ea90e246bf4f57193841e9b9a6c78e6b478937
Max <makshostroyshko@gmail.com>
2019-01-09 17:58:55 +0200

    added ceo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
 
   def full_title(page_title = '')
-    base_title = "GHShop"
+    base_title = "Shop"
     if page_title.empty?
       base_title
     else
-      base_title + ' | ' + page_title
+      page_title
     end
   end
 end
