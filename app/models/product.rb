b05eeac14020bcae7cc25fb4fb3ed6731ab0ae2f
Max <makshostroyshko@gmail.com>
2019-01-09 18:03:31 +0200

    some fixed

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,11 +4,11 @@ class Product < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :order_items
 
-  validates :title, presence: true, length: { minimum: 3, maximum: 30 }
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :result, presence: true
   validates :price, presence: true
   # validates :pictures, presence: true
-  validates :description, presence: true, length: { minimum: 10, maximum: 500 }
+  validates :description, presence: true, length: { minimum: 10 }
 
   scope :published, -> { where(published: true) }
   default_scope { where(active: true) }
@@ -16,7 +16,7 @@ class Product < ApplicationRecord
 
   mount_uploaders :pictures, PictureUploader
 
-  def self.search(search)
+  def search(search)
     where("title ILIKE ?", "%#{search}%")
   end
 
