c85dd6f00318e75badd7042c370c255e0e1b7ef5
Max <makshostroyshko@gmail.com>
2019-01-13 00:53:36 +0200

    some changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_08_212010) do
+ActiveRecord::Schema.define(version: 2019_01_12_225104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,6 @@ ActiveRecord::Schema.define(version: 2019_01_08_212010) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
-    t.integer "position"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
